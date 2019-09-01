0860b81278c58ff6b9980b05e8dffdd38f5acdbc
Michael Rosario <michael.p.rosario@gmail.com>
2019-08-31 22:05:22 -0400

    Add minor edits

--- src/core/services/Responses.ts
+++ src/core/services/Responses.ts
@@ -2,10 +2,9 @@ import * as HttpStatus from "http-status-codes";
 import { IGenericResponse } from "../interfaces/IGenericResponse";
 
 export class Responses {
-    public static getResponse(message: string, code: number): IGenericResponse {
-        const response: IGenericResponse = { message: "", code: 20, recordId: "", errors: [], data: null };
+    public static getResponse(message: string, responseCode: number): IGenericResponse {
+        const response: IGenericResponse = { message: "", code: responseCode, recordId: "", errors: [], data: null };
         response.message = message;
-        response.code = code;
         return response;
     }
 
